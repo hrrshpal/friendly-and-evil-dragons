8d8d45211ae2f0922f1cb71d7ed9135790c8e0bb
harsh <hrrshpal@gmail.com>
2024-08-05 18:23:00 +0530

    phase 1 finished, defined dragon class

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,26 @@
 // Your code here
+class Dragon{
+  constructor(name,color){
+    this.name = name;
+    this.color = color;
+  }
 
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons){
+    let arr = []
+    for(let dragon of dragons){
+      arr.push(dragon.name)
+    }
+    return arr
+  }
+}
+
+const puff = new Dragon("Puff", "green");
+const toothless = new Dragon("Toothless", "black");
+console.log(Dragon.getDragons(puff, toothless));
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
